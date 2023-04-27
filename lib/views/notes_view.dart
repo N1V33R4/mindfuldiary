a81a12f01f481765a1a46e23c7fe85f5eca24ef2
Hou Neavireakpong <neavireakpong.hou@gmail.com>
2023-04-27 13:25:02 +0700

    Inject notesSerivce and use StreamBuilder

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,9 @@
+import 'dart:wasm';
+
 import 'package:flutter/material.dart';
 import 'package:mindfuldiary/constants/routes.dart';
 import 'package:mindfuldiary/services/auth/auth_service.dart';
+import 'package:mindfuldiary/services/crud/notes_service.dart';
 
 import '../enums/menu_actions.dart';
 
@@ -12,6 +15,22 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
+  late final NotesService _notesService;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _notesService.open();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,6 +59,27 @@ class _NotesViewState extends State<NotesView> {
             ],
           ),
         ],
+      ),
+      body: FutureBuilder(
+        future: _notesService.getOrCreateUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return StreamBuilder(
+                stream: _notesService.allNotes,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const Text("Waiting for all notes");
+                    default:
+                      return const CircularProgressIndicator();
+                  }
+                },
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
