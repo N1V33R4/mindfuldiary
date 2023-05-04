2a2934a1b33b71f113a8f43223ed7f0229fdb672
Hou Neavireakpong <neavireakpong.hou@gmail.com>
2023-05-04 12:19:01 +0700

    Refactored ListView to new NotesListView

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+
 import 'package:mindfuldiary/constants/routes.dart';
 import 'package:mindfuldiary/services/auth/auth_service.dart';
 import 'package:mindfuldiary/services/crud/notes_service.dart';
-
-import '../../enums/menu_actions.dart';
+import 'package:mindfuldiary/utilities/logout_dialog.dart';
+import 'package:mindfuldiary/views/notes/notes_list_view.dart';
+import 'package:mindfuldiary/enums/menu_actions.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -76,24 +78,10 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final allNotes = snapshot.data as List<DatabaseNote>;
-                        return ListView.builder(
-                          itemCount: allNotes.length,
-                          itemBuilder: (context, index) {
-                            final note = allNotes[index];
-                            return ListTile(
-                              title: Text(
-                                note.title,
-                                maxLines: 1,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              subtitle: Text(
-                                note.text,
-                                maxLines: 2,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            );
+                        return NotesListView(
+                          notes: allNotes,
+                          onDeleteNote: (note) async {
+                            await _notesService.deleteNote(id: note.id);
                           },
                         );
                       }
@@ -110,24 +98,4 @@ class _NotesViewState extends State<NotesView> {
       ),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text('Sign out'),
-      content: const Text('Are you sure you want to sign out?'),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(false),
-          child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(true),
-          child: const Text('Logout'),
-        ),
-      ],
-    ),
-  ).then((value) => value ?? false);
 }
